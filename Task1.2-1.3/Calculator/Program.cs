b1a30077e43a8cb0048995ca2e702191a39cd39e
Xiaoyi Chen <104607544@student.swin.edu.au>
2026-08-18 22:06:40 +1000

    Upload

---
+++ Task1.2-1.3/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Humanizer;
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a calculator for addition");
+            Console.WriteLine("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            double sum = num1 + num2;
+            Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+            method();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void method()
+        {
+            var appInformation = new
+            {
+                Name = "Calculator",
+                Version = 1
+            };
+
+            string json = JsonConvert.SerializeObject(appInformation);
+            string title = "calculator".Humanize(LetterCasing.Title);
+            Console.WriteLine(title + "\n" + json);
+        }
+    }
+}
